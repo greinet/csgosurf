9d66ce52b2d7e601edff54765fbb5ec2cd4a9197
greinet <57115527+greinet@users.noreply.github.com>
2020-05-18 21:35:32 +0200

    Debug messages

--- startup.sh
+++ startup.sh
@@ -9,24 +9,33 @@ if [ ! -f "/steam/pluginmarker" ]; then
   echo "Installing plugins"
   cd /steam/csgo/csgo
   #SM und MM
+  echo "Installing Sourcemod and Metamod"
   curl -sqL "https://mms.alliedmods.net/mmsdrop/1.10/mmsource-1.10.7-git971-linux.tar.gz"  | tar xz -C /steam/csgo/csgo
   curl -sqL "https://sm.alliedmods.net/smdrop/1.10/sourcemod-1.10.0-git6488-linux.tar.gz"  | tar xz -C /steam/csgo/csgo
   #DHooks
+  echo "Installing DHooks"
   wget -O dhooks.zip  https://github.com/peace-maker/DHooks2/releases/download/v2.2.0-detours10/dhooks-2.2.0-detours10-sm110.zip && unzip dhooks.zip && rm dhooks.zip
   #SMJansson
+  echo "Installing SMJansson"
   wget -O /steam/csgo/csgo/addons/sourcemod/extensions/smjansson.ext.so https://github.com/JoinedSenses/SMJansson/raw/master/bin/smjansson.ext.so
   #SteamWorks
+  echo "Installing SteamWorks"
   curl -sqL "http://users.alliedmods.net/~kyles/builds/SteamWorks/SteamWorks-git132-linux.tar.gz"  | tar xz -C /steam/csgo/csgo
   #Discord Api
+  echo "Installing Discord Api"
   wget -O /steam/csgo/csgo/addons/sourcemod/plugins/discord_api.smx https://github.com/surftimer/Surftimer-olokos/releases/download/285/discord_api.smx
   #Surftimer
+  echo "Installing Surftimer"
   wget -O /steam/csgo/csgo/addons/sourcemod/plugins/discord_api.smx https://github.com/surftimer/Surftimer-olokos/releases/download/285/discord_api.smx
   #Cleaner
+  echo "Installing Cleaner"
   wget -O /steam/csgo/csgo/addons/sourcemod/extensions/cleaner.ext.2.csgo.so https://github.com/Accelerator74/Cleaner/raw/master/Release/cleaner.ext.2.csgo.so
   #Database configuration
+  echo "Installing Database configuration"
   wget -O /steam/csgo/csgo/addons/soourcemod/configs/databases.cfg https://raw.githubusercontent.com/greinet/csgosurf/master/databases.cfg
   
   cd /steam/csgo/
+  echo "Finished installing plugins"
 fi
 
 # Start csgo
