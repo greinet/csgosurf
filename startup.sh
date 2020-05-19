0cf1e3f9b223a3f045b7c16c9bbca9e725cd80a9
greinet <57115527+greinet@users.noreply.github.com>
2020-05-19 08:53:03 +0200

    Testing plugin installation seperate folder

--- startup.sh
+++ startup.sh
@@ -6,12 +6,13 @@
 # Install plugins
 if [ ! -f "/steam/pluginmarker" ]; then
   touch /steam/pluginmarker
+  mkdir /steam/plugins
   echo "Installing plugins"
-  cd /steam/csgo/csgo
+  cd /steam/plugins
   #SM und MM
   echo "Installing Sourcemod and Metamod"
-  curl -sqL "https://mms.alliedmods.net/mmsdrop/1.10/mmsource-1.10.7-git971-linux.tar.gz"  | tar xz -C /steam/csgo/csgo
-  curl -sqL "https://sm.alliedmods.net/smdrop/1.10/sourcemod-1.10.0-git6488-linux.tar.gz"  | tar xz -C /steam/csgo/csgo
+  curl -sqL "https://mms.alliedmods.net/mmsdrop/1.10/mmsource-1.10.7-git971-linux.tar.gz"  | tar xz -C /steam/plugins/
+  curl -sqL "https://sm.alliedmods.net/smdrop/1.10/sourcemod-1.10.0-git6488-linux.tar.gz"  | tar xz -C /steam/plugins/
   #DHooks
   echo "Installing DHooks"
   wget -O dhooks.zip  https://github.com/peace-maker/DHooks2/releases/download/v2.2.0-detours10/dhooks-2.2.0-detours10-sm110.zip
@@ -21,19 +22,19 @@ if [ ! -f "/steam/pluginmarker" ]; then
   rm dhooks.zip
   #SMJansson
   echo "Installing SMJansson"
-  wget -O /steam/csgo/csgo/addons/sourcemod/extensions/smjansson.ext.so https://github.com/JoinedSenses/SMJansson/raw/master/bin/smjansson.ext.so
+  wget -O /steam/plugins/addons/sourcemod/extensions/smjansson.ext.so https://github.com/JoinedSenses/SMJansson/raw/master/bin/smjansson.ext.so
   #SteamWorks
   echo "Installing SteamWorks"
-  curl -sqL "http://users.alliedmods.net/~kyles/builds/SteamWorks/SteamWorks-git132-linux.tar.gz"  | tar xz -C /steam/csgo/csgo
+  curl -sqL "http://users.alliedmods.net/~kyles/builds/SteamWorks/SteamWorks-git132-linux.tar.gz"  | tar xz -C /steam/plugins/
   #Discord Api
   echo "Installing Discord Api"
-  wget -O /steam/csgo/csgo/addons/sourcemod/plugins/discord_api.smx https://github.com/surftimer/Surftimer-olokos/releases/download/285/discord_api.smx
+  wget -O /steam/plugins/addons/sourcemod/plugins/discord_api.smx https://github.com/surftimer/Surftimer-olokos/releases/download/285/discord_api.smx
   #Cleaner
   echo "Installing Cleaner"
-  wget -O /steam/csgo/csgo/addons/sourcemod/extensions/cleaner.ext.2.csgo.so https://github.com/Accelerator74/Cleaner/raw/master/Release/cleaner.ext.2.csgo.so
+  wget -O /steam/plugins/addons/sourcemod/extensions/cleaner.ext.2.csgo.so https://github.com/Accelerator74/Cleaner/raw/master/Release/cleaner.ext.2.csgo.so
   #Database configuration
   echo "Installing Database configuration"
-  wget -O /steam/csgo/csgo/addons/sourcemod/configs/databases.cfg https://raw.githubusercontent.com/greinet/csgosurf/master/databases.cfg
+  wget -O /steam/plugins/addons/sourcemod/configs/databases.cfg https://raw.githubusercontent.com/greinet/csgosurf/master/databases.cfg
   #Surftimer
   echo "Installing SurfTimer"
   wget -O timer.zip  https://github.com/surftimer/Surftimer-olokos/archive/dev.zip
@@ -41,7 +42,7 @@ if [ ! -f "/steam/pluginmarker" ]; then
   unzip -o timer.zip
   echo "unzip done"
   rm timer.zip
-  wget -O /steam/csgo/csgo/addons/sourcemod/plugins/discord_api.smx https://github.com/surftimer/Surftimer-olokos/releases/download/285/SurfTimer.smx
+  wget -O /steam/plugins/addons/sourcemod/plugins/discord_api.smx https://github.com/surftimer/Surftimer-olokos/releases/download/285/SurfTimer.smx
   
   cd /steam/csgo/
   echo "Finished installing plugins"
